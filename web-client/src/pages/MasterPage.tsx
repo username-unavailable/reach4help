ebe9dd08a04f286a8555c9b57356eaee0fc0b162
puzzledbytheweb <pmsfilipe1@gmail.com>
2020-04-01 02:31:52 +0100

    feat(new-request): create simple page

--- web-client/src/pages/MasterPage.tsx
+++ web-client/src/pages/MasterPage.tsx
@@ -1,15 +1,20 @@
-import React, { ReactElement } from 'react';
-import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
+import React, { ReactElement } from "react";
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 
-import ContentPage from './ContentPage';
-import ProtectedPage from './ProtectedPage';
-import { LOGIN_PATH } from './routes/LoginRoute/constants';
-import LoginRoute from './routes/LoginRoute/LoginRoute';
-import NotFoundRoute from './routes/NotFoundRoute';
+import ContentPage from "./ContentPage";
+import ProtectedPage from "./ProtectedPage";
+import { LOGIN_PATH } from "./routes/LoginRoute/constants";
+import LoginRoute from "./routes/LoginRoute/LoginRoute";
+import { NEW_REQUEST_PATH } from "./routes/NewRequestRoute/constants";
+import NewRequestRoute from "./routes/NewRequestRoute/NewRequestRoute";
+import NotFoundRoute from "./routes/NotFoundRoute";
 
 const MasterPage = (): ReactElement => (
   <Router>
     <Switch>
+      <Route exact path={NEW_REQUEST_PATH}>
+        <NewRequestRoute />
+      </Route>
       <Route exact path={LOGIN_PATH}>
         <LoginRoute />
       </Route>
